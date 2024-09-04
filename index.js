64da1fe54d9c617905cf953a57c6e0578cab4488
matnello <121647738+matnello@users.noreply.github.com>
2024-09-04 09:15:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+/* tabuada no console de 0 a 10 no formato:
+multiplicador x número = resultado  */
+
+const multiplicador = 7;
+
+for(let i = 0; i <= 10; i ++){
+    const calc = multiplicador * i;
+    console.log(`${multiplicador} x ${i} = ${calc}`);
+};
